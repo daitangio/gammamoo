d5fe893c0edf169ed45fc937af6bcdcab33d0726
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2021-01-08 18:52:31 +0100

    Simple room creation does not work quite well :(

--- db-cores/scripting/simple-setup.sh
+++ db-cores/scripting/simple-setup.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 echo Simple core Setup
-exec 5<>/dev/tcp/127.0.0.1/7777
 
 # 62 is the first room It seems
 # The Noth park seems 97
-cat >&5 <<EOF
-connect wizard
-EOF
 
-
-
-for f in $(dirname $0)/1*.txt; do
+# $(dirname $0)/1*.txt
+for f in $@; do
     echo Loading ${f}...
+    exec 5<>/dev/tcp/127.0.0.1/7777
     cat >&5 < $f    
+    cat <&5
 done
 echo Quitting...
 
-cat >&5 <<EOF
 
-;"Quit from $0"
-@quit
-EOF
 
-cat <&5
+
